cb5f24267fbf08cb61d76da7a68083d28d49dba2
francois <francois.pacaud@cermics.enpc.fr>
2016-04-04 22:31:39 +0200

    [UPD] Use only Flask SQL alchemy to init database

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
 DB_NAME = "app.db"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(basedir, 'app.db')
 
 PORT = 5000
 DEBUG = True
--- load_bibfile.py
+++ load_bibfile.py
@@ -12,27 +12,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# from apps.models import BiblioEntry
+from apps.models import BiblioEntry
 
-Base = declarative_base()
-
-
-#TODO: create an instance with a single dict
-class BiblioEntry(Base):
-    __tablename__ = "Biblio"
-    ID = Column(String, primary_key=True)
-    ENTRYTYPE = Column(String)
-    authors = Column(String)
-    title = Column(String)
-    year = Column(Integer)
-    month = Column(String)
-    publisher = Column(String)
-    journal = Column(String)
-    school = Column(String)
-    pdf = Column(String)
-    url = Column(String)
-    keywords = Column(String)
-
+from apps import db
 
 
 def load_bibtex():
@@ -55,15 +37,6 @@ def main():
         sys.exit(-1)
 
     print("Init database with ", sys.argv[1])
-
-    engine = create_engine('sqlite:///app.db', echo=True)
-    # Create the database:
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-
-    session = Session()
     with open(sys.argv[1]) as bibtex_file:
         bibtex_str = bibtex_file.read()
 
@@ -75,7 +48,8 @@ def main():
 
     print("Write database ...")
     for bib in bib_database.entries:
-        bib_entry = BiblioEntry(ID=bib.get("ID", ""),
+        try:
+            bib_entry = BiblioEntry(ID=bib.get("ID", ""),
                                 ENTRYTYPE=bib.get("ENTRYTYPE", ""),
                                 authors=bib.get("author", ""),
                                 title=bib.get("title", ""),
@@ -87,11 +61,11 @@ def main():
                                 pdf=bib.get("pdf", ""),
                                 url=bib.get("url", ""),
                                 keywords=bib.get("keywords", ""))
+        except:
+            print("Entry already in database: ", bib.get("ENTRYTYPE"))
+        db.session.add(bib_entry)
+    db.session.commit()
 
-        session.add(bib_entry)
-    session.commit()
-
-    session.close()
     print("Done")
 
 if __name__ == '__main__':
